0ff7e21cf9c86041231c1bd83835aa07bbcf52fb
Daniel <790119+DanTheMan827@users.noreply.github.com>
2025-03-27 00:04:57 -0500

    Enable WebView debugging

--- MBF Launcher/Platforms/Android/MainActivity.cs
+++ MBF Launcher/Platforms/Android/MainActivity.cs
@@ -1,10 +1,16 @@
 ﻿using Android.App;
 using Android.Content.PM;
+using Android.OS;
 
 namespace MBF_Launcher
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Landscape, LaunchMode = LaunchMode.SingleInstance, ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Android.Webkit.WebView.SetWebContentsDebuggingEnabled(true);
+        }
     }
 }
