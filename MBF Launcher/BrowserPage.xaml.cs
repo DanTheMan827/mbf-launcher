3a6aad86350510133cee82486eb044f52599ba1e
Daniel <790119+DanTheMan827@users.noreply.github.com>
2025-03-31 23:28:31 -0500

    Refactor BrowserPage and update Android permissions

--- MBF Launcher/BrowserPage.xaml.cs
+++ MBF Launcher/BrowserPage.xaml.cs
@@ -1,3 +1,6 @@
+using Android.Views;
+using Android.Webkit;
+
 namespace MBF_Launcher;
 
 public partial class BrowserPage : ContentPage
@@ -5,6 +8,28 @@ public partial class BrowserPage : ContentPage
     public BrowserPage()
     {
         InitializeComponent();
+        webView.HandlerChanged += this.WebView_HandlerChanged;
+    }
+
+    private void WebView_HandlerChanged(object? sender, EventArgs e)
+    {
+        if (webView.Handler?.PlatformView is Android.Webkit.WebView browser && browser.Settings is Android.Webkit.WebSettings settings)
+        {
+            settings.UserAgentString = "MbfLauncher/1.0"; // Set user agent
+            settings.JavaScriptEnabled = true;
+            settings.AllowContentAccess = true;
+            settings.CacheMode = CacheModes.Default;
+            settings.SetSupportZoom(false);
+            settings.MediaPlaybackRequiresUserGesture = false;
+            settings.DomStorageEnabled = true;
+            settings.LoadWithOverviewMode = true;
+            settings.UseWideViewPort = true;
+            settings.AllowFileAccess = true;
+            settings.MixedContentMode = MixedContentHandling.AlwaysAllow;
+            settings.JavaScriptCanOpenWindowsAutomatically = false;
+            browser.Focusable = true;
+            browser.OverScrollMode = OverScrollMode.Never;
+        }
     }
 
     public BrowserPage(string url) : this()
@@ -35,7 +60,6 @@ public partial class BrowserPage : ContentPage
                 </body>
                 </html>"
         };
-
         webView.Source = source;
     }
 }
